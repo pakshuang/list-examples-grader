dcf83101369b9ca98c09560fb63d5f603cfedd86
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2023-11-07 13:50:09 -0800

    Add grading files

--- TestListExamples.java
+++ TestListExamples.java
@@ -9,6 +9,12 @@ class IsMoon implements StringChecker {
   }
 }
 
+class EvenStringChecker implements StringChecker {
+  public boolean checkString(String s) {
+      return s.length() % 2 == 0;
+  }
+}
+
 public class TestListExamples {
   @Test(timeout = 500)
   public void testMergeRightEnd() {
@@ -17,5 +23,20 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testFilter() {
+      List<String> input1 = Arrays.asList("a", "bb", "ccc", "dddd", "eeeee");
+      List<String> expected1 = Arrays.asList("bb", "dddd");
+      assertEquals(expected1, ListExamples.filter(input1, new EvenStringChecker()));
+  }
+
+  @Test(timeout = 500)
+  public void testMerge() {
+      List<String> input1 = Arrays.asList("a", "c", "e", "g");
+      List<String> input2 = Arrays.asList("b", "d", "f", "h");
+      List<String> expected1 = Arrays.asList("a", "b", "c", "d", "e", "f", "g", "h");
+      assertEquals(expected1, ListExamples.merge(input1, input2));
   }
 }
--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
@@ -7,6 +7,7 @@ mkdir grading-area
 
 git clone $1 student-submission
 echo 'Finished cloning'
+echo
 
 
 # Draw a picture/take notes on the directory structure that's set up after
@@ -14,3 +15,45 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+echo 'Checking for correct submitted file...'
+if [ ! -f 'student-submission/ListExamples.java' ]; then
+    echo 'ListExamples.java not found'
+    exit 1
+fi
+echo 'Found ListExamples.java'
+echo
+
+echo 'Copying files to grading area...'
+cp student-submission/ListExamples.java grading-area/
+cp TestListExamples.java grading-area/
+echo 'Finished copying'
+echo
+
+echo 'Compiling...'
+cd grading-area
+javac -cp $CPATH *.java
+echo 'Finished compiling'
+echo
+
+echo 'Running tests...'
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > result.txt
+echo 'Finished running tests'
+echo
+
+echo 'Checking results...'
+# Detect failures in result.txt
+if grep -q 'FAILURES!!!' result.txt; then
+    echo 'There were test failures'
+    grep -A 1 'FAILURES!!!' result.txt | tail -n 1 > grade.txt
+    cat grade.txt
+    
+else
+    echo 'There were no test failures'
+    grep 'OK' result.txt | grep -oE [0-9]+ > grade.txt
+    grade=$(cat grade.txt)
+    echo "Tests run: $grade"
+    
+fi
+echo 'Finished checking results'
+exit 0
